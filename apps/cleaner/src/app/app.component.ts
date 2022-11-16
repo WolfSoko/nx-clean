e94fc376dfc3f695f9f2d2aa5ca22dca45dad3eb
Wolfram Sokollek <wolframsokollek@gmail.com>
2022-11-16 17:50:14 +0100

    Initial commit

---
+++ apps/cleaner/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clean-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cleaner';
+}
