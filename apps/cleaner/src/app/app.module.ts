a07422be03d652162a8a4b74241f47d05569bdcb
Wolfram Sokollek <wolframsokollek@gmail.com>
2023-02-16 13:49:23 +0100

    feat: include lib in app component

--- apps/cleaner/src/app/app.module.ts
+++ apps/cleaner/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AwesomeComponent } from '@clean-nx/awesome';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AwesomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
